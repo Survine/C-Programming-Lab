f1bf0eb9fd69321c6977cb2a032c02475a159166
Survine <hridaybardhan18@gmail.com>
2024-02-28 20:12:26 +0530

    Vowel or Consonants

--- Question_33.c
+++ Question_33.c
@@ -3,20 +3,17 @@
 int main()
 {
     char letter;
-    printf("Enter a character :");
-    scanf("%c",&letter);
+    printf("Enter a character: ");
+    scanf("%c", &letter);
 
-    if( letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' || letter == 'A' || letter == 'E' || letter == 'I' ||letter == 'O' || letter == 'U')
-    {
-        printf("The given character is a Vowel");
-    }
-    else if ((letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z'))
-    {
-        printf("The given character is Consonant");
-    }
-    else 
-    {
-        printf("Input is a non alphabetical character");
+    if ((letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z')) {
+        if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' || letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U') {
+            printf("The given character is a Vowel");
+        } else {
+            printf("The given character is a Consonant");
+        }
+    } else {
+        printf("Input is a non-alphabetical character");
     }
 
     return 0;
