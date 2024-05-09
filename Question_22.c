85fd243635c9cadbf2084d8ee3282d7bdafe21a3
Survine <hridaybardhan18@gmail.com>
2024-05-09 21:31:02 +0530

    Temperature

--- Question_22.c
+++ Question_22.c
@@ -1,33 +1,18 @@
 #include <stdio.h>
 
-int main()
-{
-    int choice;
-    float temp,convertedtemp;
-
-    printf("1 for Celsius to Fahrenheit\n");
-    printf("2 for Fahrenheit to Celsius\n");
-    printf("Enter your choice :");
-    scanf("%d", &choice);
-
-    switch (choice)
-    {
-    case 1:
-        printf("Enter temperature in Celsius: ");
-        scanf("%f", &temp);
-        convertedtemp = (temp * 9/5) + 32;
-        printf("temperature in Fahrenheit: %.2f\n", convertedtemp);
-        break;
+int main() {
+    char choice;
+    float temp, convertedTemp;
     
-    case 2:
-        printf("Enter temperature in Fahrenheit: ");
-        scanf("%f", &temp);
-        convertedtemp = (temp - 32)*5/9;
-        printf("temperature in Celsius: %.2f\n", convertedtemp);
-        break;
-    default:
-        printf("Invalid Choice");
-    }
-    return 0;
+    printf("Enter 'C' to convert from Celsius to Fahrenheit or 'F' to convert from Fahrenheit to Celsius: ");
+    scanf(" %c", &choice);
+    
+    printf("Enter temperature: ");
+    scanf("%f", &temp);
 
+    convertedTemp = (choice == 'C' || choice == 'c') ? (temp * 9 / 5) + 32 : (temp - 32) * 5 / 9;
+    
+    printf((choice == 'C' || choice == 'c') ? "%.2f Celsius is %.2f Fahrenheit.\n" : "%.2f Fahrenheit is %.2f Celsius.\n", temp, convertedTemp);
+
+    return 0;
 }
